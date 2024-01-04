fb458e8f81d9efd4d861892ad7d6a34f4f8bbb3b
Jaewoong <dixk3458@naver.com>
2024-01-04 13:22:12 +0900

    Not Found 페이지

---
+++ src/app/products/[slug]/not-found.tsx
@@ -0,0 +1,3 @@
+export default function ProductNotFoundPage() {
+  return <h1>해당 제품을 찾을수 없습니다.</h1>;
+}
--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,8 +1,15 @@
+import { notFound } from 'next/navigation';
+
 type Props = {
   params: { slug: string };
 };
 
+const products = ['pants', 'skirt'];
+
 export default function ProductPage({ params }: Props) {
+  if (!products.includes(params.slug)) {
+    notFound();
+  }
   return (
     <>
       <h1>{params.slug} page</h1>
@@ -11,7 +18,6 @@ export default function ProductPage({ params }: Props) {
 }
 
 export function generateStaticParams() {
-  const products = ['pants', 'skirt'];
   return products.map(product => ({
     slug: product,
   }));
